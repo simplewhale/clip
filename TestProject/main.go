70ee586e8e4198247f917c2683c31ff34fd86f4f
simplewhale <36060515+simplewhale@users.noreply.github.com>
2023-04-06 11:41:51 +0800

    Update main.go
    delete other test code

--- TestProject/main.go
+++ TestProject/main.go
@@ -95,37 +95,7 @@ func hexToBigInt(hex string) *big.Int {
 }
 
 func main() {
-	//c = cache.New(cache.NoExpiration,cache.NoExpiration)
-	//c.Set("1","1",cache.NoExpiration)
-	//go test()
-	//time.Sleep(10*time.Second)
-	//tokenAddress := "CW2sMRF3JJ7q8rqamJz3iZcdPRNiv3RYKDQ4LfKTkUm7"
-	//var url string
-	//url = `https://api.solscan.io/token/meta?token=%s`
-	//url = fmt.Sprintf(url,tokenAddress)
-	//result,err := Get(url)
-	//if err != nil{
-	//	fmt.Println(err)
-	//}
-	//decimal := jsoniter.Get(result,"data","decimals").ToInt()
-	//fmt.Println("精度：",decimal)
-	/*log.Println("开始测试...")
-	g := golimit.NewGoLimit(2) //max_num(最大允许并发数)设置为2
-	for i := 0; i < 10; i++ {
-		//并发计数加1.若 计数>=max_num, 则阻塞,直到 计数<max_num
-		g.Add()
-		//运行过程中可以随时修改最大可并发数据
-		//g.SetMax(3)
-		go func(g *golimit.GoLimit, i int) {
-			defer g.Done() //并发计数减1
-			time.Sleep(time.Second * 2)
-			log.Println(i, "done")
-		}(g, i)
-	}
-	log.Println("循环结束")
-	g.WaitZero() //阻塞, 直到所有并发都完成
-	log.Println("测试结束")*/
-	//pj := jsoniter.Get([]byte(`{"Image":{"URL":"http://example.com/example.gif"}}`), "Image","URL")
+	
 	for {
 		common.Start()
 	}
